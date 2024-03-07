e4edcd8b6e11f1ee8fc205bba3d76606d84b6306
Felipe Londono <felipe.londono@pragma.com.co>
2024-03-07 14:03:12 -0500

    env

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -1,10 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+const token = import.meta.env.CONTENTFUL_ACCESS_TOKEN;
 
 const client = new ApolloClient({
   uri: "https://graphql.contentful.com/content/v1/spaces/y3qdch79qxiw/environments/master",
   cache: new InMemoryCache(),
   headers: {
-    Authorization: "Bearer kJnowSU145qeU6n7NqQbuZaoZyEhmcnYQSnfh0WcV7A",
+    Authorization: `Bearer ${token}`,
     "content-type": "application/json",
   },
 });
